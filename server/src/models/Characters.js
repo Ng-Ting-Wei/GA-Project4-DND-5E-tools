fa4f5927d92a9050f991169bca27a9b8666279ea
Xenousss <ttng001@gmail.com>
2024-04-22 10:18:13 +0800

    Create Characters.js

---
+++ server/src/models/Characters.js
@@ -0,0 +1,26 @@
+const mongoose = require("mongoose");
+
+const CharacterSchema = new mongoose.Schema(
+  {
+    name: { type: String, require: true },
+    race: { type: String, require: true },
+    class: { type: String, require: true },
+    subclass: { type: String },
+    level: { type: Number, default: 1 },
+    proficiencybonus: { type: Number, default: 2 },
+    background: { type: String, require: true },
+    strength: { type: Number, require: true },
+    dexterity: { type: Number, require: true },
+    constitution: { type: Number, require: true },
+    intelligence: { type: Number, require: true },
+    wisdom: { type: Number, require: true },
+    charisma: { type: Number, require: true },
+    hitpoints: { type: Number, require: true },
+    temporaryhitpoints: { type: Number, default: 0 },
+    armorclass: { type: Number, default: 10 },
+    created_at: { type: Date, default: Date.now },
+  },
+  { collection: "character" }
+);
+
+module.exports = mongoose.model("Character", CharacterSchema);
