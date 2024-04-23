523ef8e013ac0d5351c98ff705577f66c2232fd3
Xenousss <ttng001@gmail.com>
2024-04-23 14:07:49 +0800

    Update characters.js

--- server/src/routers/characters.js
+++ server/src/routers/characters.js
@@ -2,16 +2,18 @@ const express = require("express");
 const {
   getAllCharacters,
   getCharacterById,
+  getCharacterByPlayer,
   addCharacter,
   updateCharacter,
   deleteCharacter,
 } = require("../controllers/characters");
 const router = express.Router();
 
-router.get("/characters/allcharacters", getAllCharacters);
+router.get("/characters", getAllCharacters);
 router.post("/characters/id", getCharacterById);
-router.put("/characters/addcharacters", addCharacter);
-router.patch("/characters/updatecharacters", updateCharacter);
-router.delete("/characters/deletecharacters", deleteCharacter);
+router.post("/characters/player", getCharacterByPlayer);
+router.put("/characters", addCharacter);
+router.patch("/characters", updateCharacter);
+router.delete("/characters", deleteCharacter);
 
 module.exports = router;
