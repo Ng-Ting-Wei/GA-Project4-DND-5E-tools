0d0792aae5b03b53308766f156bff3631165a6cd
Xenousss <ttng001@gmail.com>
2024-04-23 09:54:39 +0800

    Update characters.js

--- server/src/controllers/characters.js
+++ server/src/controllers/characters.js
@@ -17,6 +17,43 @@ const getAllCharacters = async (req, res) => {
   }
 };
 
+const addCharacter = async (req, res) => {
+  try {
+    const newCharacter = {
+      name: req.body.name,
+      race: req.body.race,
+      class: req.body.class,
+      level: req.body.level,
+      background: req.body.background,
+      strength: req.body.strength,
+      dexterity: req.body.dexterity,
+      constitution: req.body.constitution,
+      intelligence: req.body.intelligence,
+      wisdom: req.body.wisdom,
+      charisma: req.body.charisma,
+      hitpoints: req.bod.hitpointsy,
+      temporaryhitpoints: req.body.temporaryhitpoints,
+      armorclass: req.body.armorclass,
+      inventory: req.body.inventory,
+      player: req.body.player,
+    };
+    const character = await CharactersModal.create(newCharacter);
+
+    let user = await UsersModel.findById(req.body.player);
+
+    user.characters.push(character._id);
+
+    res.json({
+      status: "ok",
+      msg: "character saved",
+      data: { character, user },
+    });
+  } catch (error) {
+    console.error(error.message);
+    res.status(400).json({ status: "error", msg: "Error in adding character" });
+  }
+};
+
 module.exports = {
   getAllCharacters,
 };
