7ebaefab16966d6f04715922b6599cd35170bf35
Xenousss <ttng001@gmail.com>
2024-04-25 12:51:40 +0800

    Update Classlist.js

--- server/src/models/Classlist.js
+++ server/src/models/Classlist.js
@@ -3,6 +3,7 @@ const mongoose = require("mongoose");
 const ClasslistSchema = new mongoose.Schema(
   {
     classlist: { type: String, require: true },
+    detail: { type: [String] },
     created_at: { type: Date, default: Date.now },
   },
   { collection: "classlist" }
