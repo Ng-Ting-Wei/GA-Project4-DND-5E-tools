1ff7a8bf1d93e484a2e764de0abcafaa2812d852
Xenousss <ttng001@gmail.com>
2024-04-25 12:42:23 +0800

    Create Classlist.js

---
+++ server/src/models/Classlist.js
@@ -0,0 +1,11 @@
+const mongoose = require("mongoose");
+
+const ClasslistSchema = new mongoose.Schema(
+  {
+    classlist: { type: String, require: true },
+    created_at: { type: Date, default: Date.now },
+  },
+  { collection: "classlist" }
+);
+
+module.exports = mongoose.model("Classlist", ClasslistSchema);
