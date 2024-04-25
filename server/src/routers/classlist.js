14c68f0fa6082fc1f9f0c32ccdc206d333fe3a3f
Xenousss <ttng001@gmail.com>
2024-04-25 21:40:07 +0800

    Update classlist.js

--- server/src/routers/classlist.js
+++ server/src/routers/classlist.js
@@ -1,8 +1,13 @@
 const express = require("express");
 const router = express.Router();
-const { getAllClasslist, addClasses } = require("../controllers/classlist");
+const {
+  getAllClasslist,
+  addClasses,
+  getClassByName,
+} = require("../controllers/classlist");
 
 router.get("/classlist", getAllClasslist);
 router.put("/classlist", addClasses);
+router.post("/classlist", getClassByName);
 
 module.exports = router;
