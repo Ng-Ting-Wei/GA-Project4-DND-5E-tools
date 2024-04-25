a68011f4349283352ed9b2176333ff96ad4db8e0
Xenousss <ttng001@gmail.com>
2024-04-25 14:19:50 +0800

    update classlist

--- server/src/controllers/classlist.js
+++ server/src/controllers/classlist.js
@@ -3,10 +3,11 @@ const Classlist = require("../models/Classlist");
 const getAllClasslist = async (req, res) => {
   try {
     const classlists = await Classlist.find();
+    // mapping classlists first then mapping classlists.detail
     res.json(
       classlists.map((item) => ({
         classlist: item.classlist,
-        details: item.detail,
+        details: item.detail.map((detailItem) => ({ detail: detailItem })),
       }))
     );
   } catch (error) {
