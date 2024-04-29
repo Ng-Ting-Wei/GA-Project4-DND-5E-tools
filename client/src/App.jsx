72deae0a97a517f7f0d2bd407a3b68c96c9d2472
Xenousss <ttng001@gmail.com>
2024-04-29 10:13:31 +0800

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -35,7 +35,7 @@ function App() {
           <Route
             path="charactercreation"
             element={<CharacterCreation />}
-          ></Route>{" "}
+          ></Route>
           <Route path="characterview" element={<CharacterView />}></Route>
           <Route path="*" element={<NotFound />}></Route>
         </Routes>
