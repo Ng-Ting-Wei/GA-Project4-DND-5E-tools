6d7731f7d2ca5b9ed49071afab5254c3b9a2f39d
Xenousss <ttng001@gmail.com>
2024-04-29 10:55:51 +0800

    Update info.jsx

--- client/src/context/info.jsx
+++ client/src/context/info.jsx
@@ -1,19 +1,59 @@
-import React, { createContext, useState } from "react";
+import React, { createContext, useContext, useState } from "react";
 
 const InfoContext = createContext();
 
-const InfoProvider = ({ children }) => {
+export const InfoProvider = ({ children }) => {
+  const [isLoggedIn, setIsLoggedIn] = useState(false);
+  const [token, setToken] = useState("");
+  const [userInfo, setUserInfo] = useState({});
+  const [userById, setUserById] = useState({});
+  const [userRole, setUserRole] = useState({});
   const [characterId, setCharacterId] = useState("");
+
+  const storeToken = (token) => {
+    setToken(token);
+  };
+
+  const storeUser = (user) => {
+    setUserInfo(user);
+  };
+
+  const login = (token, user) => {
+    setIsLoggedIn(true);
+    setToken(token);
+    setUserInfo(user);
+  };
+
+  const logout = () => {
+    setIsLoggedIn(false);
+    setToken("");
+    setUserInfo({});
+  };
 
   const updateCharacterID = (newCharacterId) => {
     setCharacterId(newCharacterId);
   };
 
-  return (
-    <InfoContext.Provider value={{ characterId, updateCharacterID }}>
-      {children}
-    </InfoContext.Provider>
-  );
+  const value = {
+    isLoggedIn,
+    token,
+    userInfo,
+    userById,
+    userRole,
+    characterId,
+    storeToken,
+    storeUser,
+    login,
+    logout,
+    setUserInfo,
+    setUserById,
+    setUserRole,
+    updateCharacterID,
+  };
+
+  return <InfoContext.Provider value={value}>{children}</InfoContext.Provider>;
 };
 
-export { InfoProvider, InfoContext };
+export const useInfo = () => {
+  return useContext(InfoContext);
+};
