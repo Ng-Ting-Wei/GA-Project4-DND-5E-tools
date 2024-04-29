82dd342fda5fcbb1f92da0a5bb9b6c6e80576a76
Xenousss <ttng001@gmail.com>
2024-04-29 10:11:30 +0800

    Update Player.jsx

--- client/src/pages/Player.jsx
+++ client/src/pages/Player.jsx
@@ -1,35 +1,10 @@
-import React, { useState, useContext, useEffect } from "react";
-import { useInfo } from "../context/info";
+import React, { useContext } from "react";
 import UserContext from "../context/user";
-import useFetch from "../hooks/useFetch";
 import NavBar from "../components/NavBar";
 import CharacterDisplay from "../components/CharacterDisplay";
 
 const Player = () => {
-  const {
-    isLoggedIn,
-    token,
-    userInfo,
-    userById,
-    userRole,
-    storeToken,
-    storeUser,
-    login,
-    logout,
-    setUserInfo,
-    setUserById,
-    setUserRole,
-  } = useInfo();
-
   const userCtx = useContext(UserContext);
-  const fetchData = useFetch();
-
-  const userId = userCtx.userById;
-  const test01 = userCtx.userById;
-
-  const handleTest = () => {
-    console.log(test01);
-  };
 
   return (
     <div>
