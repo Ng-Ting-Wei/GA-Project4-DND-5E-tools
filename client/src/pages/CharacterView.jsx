d9f55cdfa0fb7b61f64a852cdcc627893c417993
Xenousss <ttng001@gmail.com>
2024-04-29 15:23:37 +0800

    Update CharacterView.jsx

--- client/src/pages/CharacterView.jsx
+++ client/src/pages/CharacterView.jsx
@@ -13,6 +13,20 @@ const CharacterView = () => {
 
   const [character, setCharacter] = useState("");
   const [isEditing, setIsEditing] = useState(false);
+  const [racelist, setRacelist] = useState([]);
+  const [classlist, setClasslist] = useState([]);
+  const [backgroundlist, setBackgroundlist] = useState([]);
+  const [savingthrowslist, setSavingthrowslist] = useState([]);
+  const [skilllist, setSkilllist] = useState([]);
+  const [strength, setStrength] = useState("");
+  const [dexterity, setDexterity] = useState("");
+  const [constitution, setConsitution] = useState("");
+  const [intelligence, setIntelligence] = useState("");
+  const [wisdom, setWisdom] = useState("");
+  const [charisma, setCharisma] = useState("");
+  const [hitpoints, setHitpoints] = useState("");
+  const [armorclass, setArmorclass] = useState("");
+  const [inventory, setInventory] = useState([]);
 
   const getCharacter = async () => {
     try {
@@ -31,6 +45,51 @@ const CharacterView = () => {
       }
     } catch (error) {
       console.error("Error fetching character:", error);
+    }
+  };
+
+  const getClasslist = async () => {
+    const res = await fetchData("/api/classlist");
+    if (res.ok) {
+      setClasslist(res.data);
+    } else {
+      console.log(res.data);
+    }
+  };
+
+  const getRacelist = async () => {
+    const res = await fetchData("/api/racelist");
+    if (res.ok) {
+      setRacelist(res.data);
+    } else {
+      console.log(res.data);
+    }
+  };
+
+  const getBackgroundlist = async () => {
+    const res = await fetchData("/api/backgroundlist");
+    if (res.ok) {
+      setBackgroundlist(res.data);
+    } else {
+      console.log(res.data);
+    }
+  };
+
+  const getSavingthrowlist = async () => {
+    const res = await fetchData("/api/savingthrowlist");
+    if (res.ok) {
+      setSavingthrowslist(res.data);
+    } else {
+      console.log(res.data);
+    }
+  };
+
+  const getSkilllist = async () => {
+    const res = await fetchData("/api/skilllist");
+    if (res.ok) {
+      setSkilllist(res.data);
+    } else {
+      console.log(res.data);
     }
   };
 
@@ -78,6 +137,14 @@ const CharacterView = () => {
   };
 
   useEffect(() => {
+    getClasslist();
+    getRacelist();
+    getBackgroundlist();
+    getSavingthrowlist();
+    getSkilllist();
+  }, []);
+
+  useEffect(() => {
     getCharacter();
   }, [characterId]);
 
@@ -87,14 +154,25 @@ const CharacterView = () => {
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    setCharacter((prevCharacter) => ({
-      ...prevCharacter,
-      [name]: value,
-    }));
+
+    if (name === "feature") {
+      // Split the textarea value by newline to convert it into an array
+      const featureArray = value.split("\n");
+      setCharacter((prevCharacter) => ({
+        ...prevCharacter,
+        [name]: featureArray,
+      }));
+    } else {
+      // For other input fields, update state normally
+      setCharacter((prevCharacter) => ({
+        ...prevCharacter,
+        [name]: value,
+      }));
+    }
   };
 
+  // Reset the character state to its original values
   const handleCancel = () => {
-    // Reset the character state to its original values
     getCharacter();
     setIsEditing(false);
   };
@@ -110,6 +188,7 @@ const CharacterView = () => {
           {isEditing ? (
             <>
               <div>
+                Name:
                 <input
                   type="text"
                   name="name"
@@ -117,11 +196,168 @@ const CharacterView = () => {
                   onChange={handleChange}
                 />
               </div>
+
               <div>
+                Race:
+                <select
+                  name="race"
+                  value={character.race}
+                  onChange={handleChange}
+                >
+                  {racelist.map((item, index) => {
+                    return (
+                      <option key={index} value={item.race}>
+                        {item.race}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              <div>
+                Class:
+                <select
+                  name="class"
+                  value={character.class}
+                  onChange={handleChange}
+                >
+                  {classlist.map((item, index) => {
+                    return (
+                      <option key={index} value={item.classlist}>
+                        {item.classlist}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              <div>
+                Feature:
+                <textarea
+                  value={character.feature}
+                  name="feature"
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                Level:
                 <input
                   type="number"
                   name="level"
                   value={character.level}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                Proficiency Bonus:
+                <input
+                  type="number"
+                  name="proficiencybonus"
+                  value={character.proficiencybonus}
+                  onChange={handleChange}
+                />
+              </div>
+
+              <div>
+                Background:
+                <select
+                  name="background"
+                  value={character.background}
+                  onChange={handleChange}
+                >
+                  {backgroundlist.map((item, index) => {
+                    return (
+                      <option key={index} value={item.background}>
+                        {item.background}
+                      </option>
+                    );
+                  })}
+                </select>
+              </div>
+
+              <div>
+                Savingthrows:
+                {savingthrowslist.map((item, index) => (
+                  <label key={index}>
+                    <input
+                      type="checkbox"
+                      checked={character.savingthrows.includes(
+                        item.savingthrow
+                      )}
+                      onChange={handleChange}
+                    />
+                    {item.savingthrow}
+                  </label>
+                ))}
+              </div>
+
+              <div>
+                Skills:
+                {skilllist.map((item, index) => (
+                  <label key={index}>
+                    <input
+                      type="checkbox"
+                      checked={character.skill.includes(item.skill)}
+                      onChange={handleChange}
+                    />
+                    {item.skill}
+                  </label>
+                ))}
+              </div>
+
+              <div>
+                Strength:
+                <input
+                  type="number"
+                  name="strength"
+                  value={character.strength}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                Dexterity:
+                <input
+                  type="number"
+                  name="dexterity"
+                  value={character.dexterity}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                Constitution:
+                <input
+                  type="number"
+                  name="constitution"
+                  value={character.constitution}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                Intelligence:
+                <input
+                  type="number"
+                  name="intelligence"
+                  value={character.intelligence}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                Wsdom:
+                <input
+                  type="number"
+                  name="wisdom"
+                  value={character.wisdom}
+                  onChange={handleChange}
+                />
+              </div>
+              <div>
+                Charisma:
+                <input
+                  type="number"
+                  name="charisma"
+                  value={character.charisma}
                   onChange={handleChange}
                 />
               </div>
@@ -133,6 +369,7 @@ const CharacterView = () => {
               <h1>{character.name}</h1>
               <p>Race: {character.race}</p>
               <p>Class: {character.class}</p>
+              <p>Feature: {character.feature.join(", ")}</p>
               <p>Level: {character.level}</p>
               <p>Background: {character.background}</p>
               <p>Proficiency Bonus: {character.proficiencybonus}</p>
