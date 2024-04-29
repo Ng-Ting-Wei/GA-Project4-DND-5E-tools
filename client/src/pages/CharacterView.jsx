f0ba11430b9f1875ec5ee75a328fedaffcc03238
Xenousss <ttng001@gmail.com>
2024-04-29 10:31:02 +0800

    Update CharacterView.jsx

--- client/src/pages/CharacterView.jsx
+++ client/src/pages/CharacterView.jsx
@@ -1,6 +1,39 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import { useParams, useNavigate } from "react-router-dom";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
 
 const CharacterView = () => {
+  const userCtx = useContext(UserContext);
+  const fetchData = useFetch();
+  const navigate = useNavigate();
+  const [characterDetail, setCharacterDetail] = useState(null);
+
+  const getCharacter = async () => {
+    try {
+      const res = await fetchData(
+        // Use characterId to fetch character details
+        `/api/characters/id`,
+        "POST",
+        {
+          _id: characterId,
+        },
+        userCtx.accessToken
+      );
+      if (res.ok) {
+        setCharacter(res.data); // Set character details in state
+      } else {
+        console.error("Error fetching character:", res.data);
+      }
+    } catch (error) {
+      console.error("Error fetching character:", error);
+    }
+  };
+
+  useEffect(() => {
+    getCharacter();
+  }, []);
+
   return <div></div>;
 };
 
