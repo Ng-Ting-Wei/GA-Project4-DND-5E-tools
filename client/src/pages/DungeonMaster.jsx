a05ae3b82feb56001a03194664c63362e95023e1
Xenousss <ttng001@gmail.com>
2024-04-29 10:14:24 +0800

    Update DungeonMaster.jsx

--- client/src/pages/DungeonMaster.jsx
+++ client/src/pages/DungeonMaster.jsx
@@ -5,30 +5,7 @@ import useFetch from "../hooks/useFetch";
 import NavBar from "../components/NavBar";
 
 const DungeonMaster = () => {
-  const {
-    isLoggedIn,
-    token,
-    userInfo,
-    userById,
-    userRole,
-    storeToken,
-    storeUser,
-    login,
-    logout,
-    setUserInfo,
-    setUserById,
-    setUserRole,
-  } = useInfo();
-
   const userCtx = useContext(UserContext);
-  const fetchData = useFetch();
-
-  const userId = userCtx.userById;
-  const test01 = userCtx.userRole;
-
-  const handleTest = () => {
-    console.log(test01);
-  };
 
   return (
     <div>
