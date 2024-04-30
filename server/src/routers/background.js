bf692aa720d05b875bab072ed76870a2049e081f
Xenousss <ttng001@gmail.com>
2024-04-30 14:51:15 +0800

    Update background.js

--- server/src/routers/background.js
+++ server/src/routers/background.js
@@ -5,9 +5,26 @@ const {
   getAllBackgroundlist,
   addBackground,
 } = require("../controllers/background");
+const {
+  checkBackgroundlistInput,
+  checkBackgroundByName,
+} = require("../validators/background");
+const { errorCheck } = require("../validators/errorCheck");
 const { auth } = require("../middleware/users");
 
-router.get("/backgroundlist", auth, getAllBackgroundlist);
-router.put("/backgroundlist", auth, addBackground);
+router.get(
+  "/backgroundlist",
+  auth,
+  checkBackgroundlistInput,
+  errorCheck,
+  getAllBackgroundlist
+);
+router.put(
+  "/backgroundlist",
+  auth,
+  checkBackgroundByName,
+  errorCheck,
+  addBackground
+);
 
 module.exports = router;
