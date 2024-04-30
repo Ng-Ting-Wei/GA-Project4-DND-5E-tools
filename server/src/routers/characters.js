e4ff290926629329781c750562fe66d92cdff039
Xenousss <ttng001@gmail.com>
2024-04-30 14:36:39 +0800

    update validators

--- server/src/routers/characters.js
+++ server/src/routers/characters.js
@@ -7,13 +7,23 @@ const {
   updateCharacter,
   deleteCharacter,
 } = require("../controllers/characters");
+const { errorCheck } = require("../validators/errorCheck");
+const {
+  checkAddCharacterInput,
+  checkUpdateCharacterinput,
+} = require("../validators/characters");
 const router = express.Router();
 
 router.get("/characters", getAllCharacters);
 router.post("/characters/id", getCharacterById);
 router.post("/characters/player", getCharacterByPlayer);
-router.put("/characters", addCharacter);
-router.patch("/characters", updateCharacter);
+router.put("/characters", checkAddCharacterInput, errorCheck, addCharacter);
+router.patch(
+  "/characters",
+  checkUpdateCharacterinput,
+  errorCheck,
+  updateCharacter
+);
 router.delete("/characters", deleteCharacter);
 
 module.exports = router;
--- server/src/validators/characters.js
+++ server/src/validators/characters.js
@@ -1,57 +1,48 @@
 const { body } = require("express-validator");
 
 const checkAddCharacterInput = [
-  body("classlist", "name is required").not().isEmpty(),
-  body("race", "race is required").not().isEmpty(),
-  body("race", "race is must be character").isString(),
-  body("class", "class is required").not().isEmpty(),
-  body("class", "class is must be character").isString(),
-  body("level", "level must be a number").isInt(),
-  body("background", "background is required").not().isEmpty(),
-  body("background", "background is must be character").isString(),
-  body("savingthrows", "savingthrows is required").not().isEmpty(),
-  body("savingthrows", "savingthrows is must be character").isString(),
-  body("skill", "skill is required").not().isEmpty(),
-  body("skill", "skill is must be character").isString(),
-  body("strength", "strength must be a number").isInt(),
-  body("strength", "strength must be a number between 3 to 30").isInt({
+  body("classes", "Classes is required").not().isEmpty(),
+  body("classes", "Classes must be a string").isString(),
+  body("race", "Race is required").not().isEmpty(),
+  body("race", "Race must be a string").isString(),
+  body("level", "Level must be a number").isInt(),
+  body("background", "Background is required").not().isEmpty(),
+  body("background", "Background must be a string").isString(),
+  body("strength", "Strength must be a number").isInt({ min: 3, max: 30 }),
+  body("dexterity", "Dexterity must be a number").isInt({ min: 3, max: 30 }),
+  body("constitution", "Constitution must be a number").isInt({
     min: 3,
     max: 30,
   }),
-  body("dexterity", "dexterity must be a number").isInt(),
-  body("dexterity", "dexterity number between 3 to 30").isInt({
+  body("intelligence", "Intelligence must be a number").isInt({
     min: 3,
     max: 30,
   }),
-  ,
-  body("constitution", "constitution must be a number").isInt(),
-  body("constitution", "constitution number between 3 to 30").isInt({
-    min: 3,
-    max: 30,
-  }),
-  ,
-  body("intelligence", "intelligence must be a number").isInt({
-    min: 3,
-    max: 30,
-  }),
-  ,
-  body("intelligence", "intelligence number between 3 to 30").isInt(),
-  body("wisdom", "wisdom must be a number").isInt(),
-  body("wisdom", "wisdom number between 3 to 30").isInt({
-    min: 3,
-    max: 30,
-  }),
-  ,
-  body("charisma", "charisma must be a number").isInt(),
-  body("charisma", "charisma number between 3 to 30").isInt({
-    min: 3,
-    max: 30,
-  }),
-  ,
-  body("hitpoints", "hitpoints must be a number").isInt(),
-  body("temporaryhitpoints", "temporaryhitpoints must be a number").isInt(),
-  body("armorclass", "armorclass must be a number").isInt(),
-  body("inventory", "inventory is must be characters").isString(),
+  body("wisdom", "Wisdom must be a number").isInt({ min: 3, max: 30 }),
+  body("charisma", "Charisma must be a number").isInt({ min: 3, max: 30 }),
+  body("maximumhitpoints", "maximumhitpoints must be a number").isInt(),
+  body("currenthitpoints", "currenthitpoints must be a number").isInt(),
+  body("armorclass", "Armor Class must be a number").isInt(),
+  body("inventory", "Inventory must be a string").isArray(),
 ];
 
-module.exports = { checkAddCharacterInput };
+const checkUpdateCharacterinput = [
+  body("strength", "Strength must be a number").isInt({ min: 3, max: 30 }),
+  body("dexterity", "Dexterity must be a number").isInt({ min: 3, max: 30 }),
+  body("constitution", "Constitution must be a number").isInt({
+    min: 3,
+    max: 30,
+  }),
+  body("intelligence", "Intelligence must be a number").isInt({
+    min: 3,
+    max: 30,
+  }),
+  body("wisdom", "Wisdom must be a number").isInt({ min: 3, max: 30 }),
+  body("charisma", "Charisma must be a number").isInt({ min: 3, max: 30 }),
+  body("maximumhitpoints", "maximumhitpoints must be a number").isInt(),
+  body("currenthitpoints", "currenthitpoints must be a number").isInt(),
+  body("armorclass", "Armor Class must be a number").isInt(),
+  body("inventory", "Inventory must be a string").isArray(),
+];
+
+module.exports = { checkAddCharacterInput, checkUpdateCharacterinput };
