cad2589b64f2e99b53ba413225d4a701207e2c0a
Xenousss <ttng001@gmail.com>
2024-04-30 12:03:17 +0800

    Update NPCsDisplay.jsx

--- client/src/components/NPCsDisplay.jsx
+++ client/src/components/NPCsDisplay.jsx
@@ -1,6 +1,18 @@
-import React from "react";
+import React, { useContext, useEffect, useState } from "react";
+import useFetch from "../hooks/useFetch";
+import UserContext from "../context/user";
+import { useInfo } from "../context/info";
+import { useNavigate } from "react-router-dom";
 
 const NPCsDisplay = () => {
+  const userCtx = useContext(UserContext);
+  const fetchData = useFetch();
+  const navigate = useNavigate();
+  const userId = userCtx.userById;
+  const { setUserInfo, setUserById, setUserRole, setCharacterId, userRole } =
+    useInfo();
+  const [npcs, setNPCs] = useState([]);
+
   return <div></div>;
 };
 
