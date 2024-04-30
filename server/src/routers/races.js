906dfc8027182b750529d33c57e43dac03c1b8f2
Xenousss <ttng001@gmail.com>
2024-04-30 14:51:45 +0800

    Update races.js

--- server/src/routers/races.js
+++ server/src/routers/races.js
@@ -3,9 +3,10 @@ const router = express.Router();
 
 const { getAllRacelist, addRace } = require("../controllers/races");
 const { checkRacelistInput, checkRaceByName } = require("../validators/races");
+const { errorCheck } = require("../validators/errorCheck");
 const { auth } = require("../middleware/users");
 
-router.get("/racelist", auth, checkRacelistInput, getAllRacelist);
-router.put("/racelist", auth, checkRaceByName, addRace);
+router.get("/racelist", auth, checkRacelistInput, errorCheck, getAllRacelist);
+router.put("/racelist", auth, checkRaceByName, errorCheck, addRace);
 
 module.exports = router;
